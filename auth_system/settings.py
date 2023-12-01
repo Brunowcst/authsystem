f85cab3ba5bdd077aa97cbd12554e5411c7b8178
Brunowcst <brunoporfirio022@gmail.com>
2023-12-01 16:31:46 -0300

    Envs

--- auth_system/settings.py
+++ auth_system/settings.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-import os
 from pathlib import Path
+import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,11 +135,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.UserAccount'
 
+password_email = config('EMAIL_HOST_PASSWORD')
+
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'onurb.clive@gmail.com'
-EMAIL_HOST_PASSWORD = 'gzhc zyex fkcd hkfq'
+EMAIL_HOST_PASSWORD = password_email
 EMAIL_USE_TLS = True
 
 DJOSER = {
