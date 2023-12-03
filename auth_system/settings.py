5a0e184b57b5a5ebbd22edb64e54434542c795bd
Brunowcst <brunoporfirio022@gmail.com>
2023-12-03 01:04:52 -0300

    Atualiza configurações do cors

--- auth_system/settings.py
+++ auth_system/settings.py
@@ -33,7 +33,8 @@ ALLOWED_HOSTS = []
 # CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
 ]
 
 # Application definition
